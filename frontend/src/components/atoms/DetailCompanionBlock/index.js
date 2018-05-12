f8a513b9ac8d5172b11f4d0fbed031deae2f0ef9
SonSang <shh1295@gmail.com>
2018-05-13 02:19:48 +0900

    Implementing (DetailCompanionBlock) (1)

---
+++ frontend/src/components/atoms/DetailCompanionBlock/index.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { UncontrolledCarousel } from 'reactstrap'
+
+const items = [
+	{
+		src: 'http://bestpickr.com/wp-content/uploads/Worlds-Cutest-Small-Dog-Picture.jpg',
+		altText: 'Slide 1',
+		caption: 'Slide 1',
+		header: 'Adorable Dog'
+	},
+	{
+		src: 'http://dogsaholic.com/wp-content/uploads/2015/05/Boo-the-cutest-pomeranian-dog.jpg',
+		altText: 'Slide 2',
+		caption: 'Slide 2',
+		header: 'Cute Dog'
+	},
+	{
+		src: 'http://bestpickr.com/wp-content/uploads/Tiny-Cute-Pom-Pom-Yawning-Like-a-Kitten.jpg',
+		altText: 'Slide 3',
+		caption: 'Slide 3',
+		header: 'Lovely Dog'
+	},
+]
+
+export default ({}) => {
+	
+}
