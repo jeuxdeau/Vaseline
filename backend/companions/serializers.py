e23b665c813571ec73bd5d1415673196a7e66e31
jinwoo71 <jinwoo71@snu.ac.kr>
2018-05-31 18:06:14 +0900

    change profile serializer

--- backend/companions/serializers.py
+++ backend/companions/serializers.py
@@ -163,6 +163,11 @@ class ProfileSerializer(serializers.ModelSerializer):
     class Meta:
         model = Profile
         fields = '__all__'
+
+class ProfileUpdateSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = '__all__'
         read_only_fields = ('user',)
 
 class UserSignUpSerializer(serializers.ModelSerializer):
