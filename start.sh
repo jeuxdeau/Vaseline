0489489864cd245c2a3ac03d6047c95cfa6e16dd
Hyeonseok Choi <worldneedscat@gmail.com>
2018-06-05 21:15:59 +0900

    Modify start.sh

--- start.sh
+++ start.sh
@@ -1,8 +1,9 @@
 # Auto Server Run for Ubuntu User
+# How To Use "bash start.sh"
 
 cmds=('cd backend && python3 manage.py runserver', 'cd frontend && npm start')
 
 for i in "${cmds[@]}"
 do
-  gnome-terminal -hold -e "i" & 
+  gnome-terminal -e "i" & 
 done
