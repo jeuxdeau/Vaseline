12b6d0d197bf00edee3d8e68cfc4b7fc873cb776
jeuxdeau <keyxiz@gmail.com>
2018-06-15 15:42:35 +0900

    detach address to avoid test and apply react bootstarp to signup

--- frontend/src/components/molecules/Signup/index.js
+++ frontend/src/components/molecules/Signup/index.js
@@ -1,33 +1,15 @@
 import React, { PropTypes } from 'react'
 import styled from 'styled-components'
 import { font, palette } from 'styled-theme'
-import { Button } from '../../atoms/Button'
+import { PageHeader, Alert, Button, Dropdown, MenuItem, Jumbotron, InputGroup, Form, FormGroup, FormControl, ControlLabel, Container, Row, Col } from 'reactstrap'
+import { options } from './address.js'
+
+import TextInput from '../../atoms/TextInput'
 
 const Wrapper = styled.div`
 font-family: ${font('primary')};
 color: ${palette('grayscale', 0)};
 `
-
-const options = {
-  '서울': ['강남구','강동구','강북구','강서구','관악구','광진구','구로구','금천구','노원구','도봉구','동대문구','동작구','마포구','서대문구','서초구','성동구','성북구','송파구','양천구','영등포구','용산구','은평구','종로구','중구','중랑구'],
-  '부산': ['강서구','금정구','남구','동구','동래구','부산진구','북구','사상구','사하구','서구','수영구','연제구','영도구','중구','해운대구','기장군'],
-  '대구': ['남구','달서구','동구','북구','서구','수성구','중구','달성군'],
-  '인천': ['계양구','남구','남동구','동구','부평구','서구','연수구','중구','강화군','옹진군'],
-  '광주': ['광산구','남구','동구','북구','서구'],
-  '대전': ['대덕구','동구','서구','유성구','중구'],
-  '울산': ['남구','동구','북구','중구','울주군'],
-  '세종': ['조치원읍', '금남면', '부강면', '소정면', '연기면', '연동면', '연서면', '장군면', '전동면', '전의면', '가람동', '한솔동', '새롬동', '나성동', '다정동', '새롬동', '도담동', '어진동', '아름동', '종촌동', '고운동', '대평동', '반곡동', '보람동', '소담동'],
-  '경기': ['고양시','과천시','광명시','광주시','구리시','군포시','김포시','남양주시','동두천시','부천시','성남시','수원시','시흥시','안산시','안성시','안양시','양주시','여주시','오산시','용인시','의왕시','의정부시','이천시','파주시','평택시','포천시','하남시','화성시','가평군','양평군','연천군'],
-  '강원': ['강릉시','동해시','삼척시','속초시','원주시','춘천시','태백시','고성군','양구군','양양군','영월군','인제군','정선군','철원군','평창군','홍천군','화천군','횡성군'],
-  '충북': ['제천시','청주시','충주시','괴산군','단양군','보은군','영동군','옥천군','음성군','증평군','진천군'],
-  '충남': ['계룡시','공주시','논산시','당진시','보령시','서산시','아산시','천안시','금산군','부여군','서천군','예산군','청양군','태안군','홍성군'],
-  '전북': ['전주시','군산시','김제시','남원시','익산시','정읍시','고창군','무주군','부안군','순창군','완주군','임실군','장수군','진안군'],
-  '전남': ['광양시','나주시','목포시','순천시','여수시','강진군','고흥군','곡성군','구례군','담양군','무안군','보성군','신안군','영광군','영암군','완도군','장성군','장흥군','진도군','함평군','해남군','화순군'],
-  '경북': ['경산시','경주시','구미시','김천시','문경시','상주시','안동시','영주시','영천시','포항시','고령군','군위군','봉화군','성주군','예천군','영덕군','영양군','울릉군','울진군','의성군','청도군','청송군','칠곡군'],
-  '경남': ['거제시','김해시','밀양시','사천시','양산시','진주시','창원시','통영시','거창군','고성군','남해군','산청군','의령군','창녕군','하동군','함안군','함양군','합천군'],
-  '제주': ['제주시','서귀포시'],
-  '해외': ['해외']
-}
 
 export const SignUp = (props) => {
   /* SIGNUP POST */
@@ -116,7 +98,7 @@ export const SignUp = (props) => {
   }
 
   const onSubmit = () => {
-    /* SUBMIT TEST 
+    /* SUBMIT TEST
     console.log("==========user==========")
     console.log(username.value);
     console.log(password.value);
@@ -202,6 +184,41 @@ export const SignUp = (props) => {
   };
   return (
     <div>
+    <Container>
+      <Jumbotron>
+      <h1>Sign Up</h1>
+      <p>
+      회원님의 정보, 반려동물의 정보, 그리고 원하는 짝꿍의 모습을 알려 주세요.<br />
+      회원가입을 완료하고 마음에 꼭 드는 강아지 짝꿍을 찾아보세요.
+      </p>
+      <p>
+      <Button>Learn more</Button>
+      </p>
+      </Jumbotron>
+
+
+      <b>유저 정보</b>  당신에 대해서 알려주세요.<p/>
+      <Form onSubmit={onSubmit}>
+        <TextInput name="id" placeholder="id"/>
+        <TextInput name="password" placeholder="password"/>
+        <TextInput name="nickname" placeholder="nickname"/>
+        <TextInput name="email" placeholder="e-mail"/>
+        나이 드랍다운
+        성별 드랍다운
+        주소 드랍다운<p/>
+
+      <b>반려동물 정보</b>  여러분의 반려동물에 대해서 알려주세요.<p/>
+      가장 잘 나타내는 사진 또는 동영상을 첨부해 주세요!<br/>
+      <input type="file"/><br/>
+      <TextInput name="companion.name" placeholder="name of your companion animal"/>
+
+
+        <center><Button type="submit" color="info">다 했어요!</Button></center>
+      </Form>
+    </Container>
+
+
+
     <div><b>당신은 누구인가요?</b></div>
     아이디 <input ref={node => {username = node;}}/> <button>중복확인</button><br/>
     비밀번호 <input ref={node => {password = node;}} type="password"/><br/>
