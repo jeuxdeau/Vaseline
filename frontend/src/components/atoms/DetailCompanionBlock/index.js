7cb7b3cec024b9f611e40d35d7dfea8a130e95e3
Hyeonseok Choi <worldneedscat@gmail.com>
2018-06-18 23:52:13 +0900

    Decorate DetailPage

--- frontend/src/components/atoms/DetailCompanionBlock/index.js
+++ frontend/src/components/atoms/DetailCompanionBlock/index.js
@@ -1,45 +1,46 @@
 import React from 'react'
-import { UncontrolledCarousel } from 'reactstrap'
-import { Card, Button, CardHeader, CardFooter, CardBody, CardTitle, CardText } from 'reactstrap'
+import { Card, Col, Button, CardHeader, CardFooter, CardBody, CardTitle, CardText,  UncontrolledCarousel } from 'reactstrap'
 
 const items = [
-	{
-		src: 'http://bestpickr.com/wp-content/uploads/Worlds-Cutest-Small-Dog-Picture.jpg',
-		altText: 'Slide 1',
-		caption: 'Slide 1',
-		header: 'Adorable Dog'
-	},
-	{
-		src: 'http://dogsaholic.com/wp-content/uploads/2015/05/Boo-the-cutest-pomeranian-dog.jpg',
-		altText: 'Slide 2',
-		caption: 'Slide 2',
-		header: 'Cute Dog'
-	},
-	{
-		src: 'http://bestpickr.com/wp-content/uploads/Tiny-Cute-Pom-Pom-Yawning-Like-a-Kitten.jpg',
-		altText: 'Slide 3',
-		caption: 'Slide 3',
-		header: 'Lovely Dog'
-	},
+  {
+    src: 'http://bestpickr.com/wp-content/uploads/Worlds-Cutest-Small-Dog-Picture.jpg',
+    altText: 'Slide 1',
+    caption: 'Slide 1',
+    header: 'Adorable Dog',
+  },
+  {
+    src: 'http://dogsaholic.com/wp-content/uploads/2015/05/Boo-the-cutest-pomeranian-dog.jpg',
+    altText: 'Slide 2',
+    caption: 'Slide 2',
+    header: 'Cute Dog',
+  },
+  {
+    src: 'http://bestpickr.com/wp-content/uploads/Tiny-Cute-Pom-Pom-Yawning-Like-a-Kitten.jpg',
+    altText: 'Slide 3',
+    caption: 'Slide 3',
+    header: 'Lovely Dog',
+  },
 ]
 
-export default ({companion}) => {
-	return (
-		<div>
-			<UncontrolledCarousel items={items} />
-			<Card>
-				<CardHeader tag="h2">
-					{companion.name}{' '}
-					<Button color="info"><div>age:{companion.age}</div></Button>{' '}
-					<Button color="info"><div>sex:{companion.sex}</div></Button>{' '}
-					<Button color="info"><div>size:{companion.size}</div></Button>{' '}
-					<Button color="info"><div>breed:{companion.breed}</div></Button>{' '}
-					<Button color="info"><div>mating season:{companion.mating_season.season_start}~{companion.mating_season.season_start}</div></Button>
-				</CardHeader>
-				<CardBody>
-					<h2>Description</h2>
-				</CardBody>
-			</Card>
-		</div>
-	)
+export default ({ companion }) => {
+  return (
+    <div>
+      <Col sm="10" md={{ size: 10, offset: 1 }}>
+        <UncontrolledCarousel sm="6" items={items} />
+        <Card>
+          <CardHeader tag="h2">
+            우리 {companion.name}를 소개합니다! {' '}
+          </CardHeader>
+          <CardBody>
+            <Button color="info" size="lg"><div>나이: { 2019 - companion.birth_year}</div></Button>{' '}<br /><br />
+            <Button color="info" size="lg"><div>성별: {companion.sex}</div></Button>{' '}<br /><br />
+            <Button color="info" size="lg"><div>사이즈: {companion.size}</div></Button>{' '}<br /><br />
+            <Button color="info" size="lg"><div>품종: {companion.breed}</div></Button>{' '}<br /><br />
+            <Button color="info" size="lg"><div>이 때 만날래요: {companion.mating_season.season_start} ~ {companion.mating_season.season_start}</div></Button>
+
+          </CardBody>
+        </Card>
+      </Col>
+    </div>
+  )
 }
--- frontend/src/components/pages/DetailPage/index.js
+++ frontend/src/components/pages/DetailPage/index.js
@@ -1,79 +1,111 @@
 import React, { Component } from 'react'
-import { Jumbotron, Alert, Button, Modal } from 'reactstrap'
+import { Card, Col, Jumbotron, Alert, Button, Modal, Row, UncontrolledTooltip } from 'reactstrap'
 import DetailCompanionBlock from '../../atoms/DetailCompanionBlock'
 import Sidebar from '../../molecules/Sidebar'
 import MessageApp from '../../atoms/MessageApp'
 
 export default class DetailPage extends Component {
-	constructor(props) {
-		super(props)
-		this.state = {
-			messageAppActivated: false,
-		}
-	}
+  constructor(props) {
+    super(props)
+    this.state = {
+      messageAppActivated: false,
+    }
+  }
 
-	onSendMessageBtnClick() {
-		this.setState({
-			messageAppActivated: !this.state.messageAppActivated
-		})
-	}
+  onSendMessageBtnClick() {
+    this.setState({
+      messageAppActivated: !this.state.messageAppActivated
+    })
+  }
 
-	onSendLikeBtnClick(sender, receiver) {
-		this.props.post_like(sender, receiver)
-	}
+  onSendLikeBtnClick(sender, receiver) {
+    this.props.post_like(sender, receiver)
+  }
 
-	onSendProposalBtnClick(sender, receiver) {
-		this.props.post_proposal(sender, receiver)
-	}
+  onSendProposalBtnClick(sender, receiver) {
+    this.props.post_proposal(sender, receiver)
+  }
 
-	onSignoutBtnClick() {
-		this.props.post_signout()
-	}
+  onSignoutBtnClick() {
+    this.props.post_signout()
+  }
 
-	componentDidMount() {
-		this.props.get_companion_list()
-	}
+  componentDidMount() {
+    this.props.get_companion_list()
+  }
 
-	render() {
-		const companion_list = this.props.companion_list
-		const name = this.props.match.params.name
-		const errors = this.props.errors || {}
-		
-		if(companion_list) {
-			const companion = companion_list.find((element) => (element.name == name))
-			return (
-				<div>
-				<Jumbotron className="container">
-					<h1>
-						VASELINE 
-						<Button size="sm" outline color="primary" onClick={()=>this.onSignoutBtnClick()}>Logout</Button>{' '}
-						<Button size="sm" outline color="primary" onClick={()=>this.onSendLikeBtnClick(this.props.user_id, companion.id)}>좋아요</Button>{' '}
-						<Button size="sm" outline color="primary" onClick={()=>this.onSendMessageBtnClick()}>쪽지보내기</Button>{' '}
-						<Button size="sm" outline color="primary" onClick={()=>this.onSendProposalBtnClick(this.props.user_id, companion.id)}>결혼해요</Button>
-						<MessageApp messageAppOpen={this.state.messageAppActivated}
-									messageSenderId={this.props.user_id} 
-									messageReceiverName={companion.name} 
-									messageReceiverId={companion.id}
-									messageToggle={()=>this.onSendMessageBtnClick()}
-									messageSend={this.props.post_message}/>
-					</h1>
-					{
-						errors.get_list_errors?
-							<Alert color="danger">
-								{errors.get_list_errors}
-							</Alert>
-							:""
-					}
-					<DetailCompanionBlock companion={companion} />
-				</Jumbotron>
-				</div>
-			)
-		}
-		else {
-			return (
-				<Jumbotron className="container"> 
-				</Jumbotron>
-			)
-		}
-	}
+  render() {
+    const companion_list = this.props.companion_list
+    const name = this.props.match.params.name
+    const errors = this.props.errors || {}
+    
+    if(companion_list) {
+      const companion = companion_list.find((element) => (element.name == name))
+      return (
+        <div>
+          <Jumbotron className="container">
+            <h1>
+              VASELINE
+              <Button size="sm" outline color="primary" onClick={() => this.onSignoutBtnClick()}>Logout</Button>{' '}
+            </h1>
+            {
+              errors.get_list_errors ?
+                <Alert color="danger">
+                  {errors.get_list_errors}
+                </Alert>
+                : ''
+            }
+            <DetailCompanionBlock companion={companion} />
+            <Col sm="10" md={{ size: 10, offset: 1 }}>
+              <Card>
+                <div>
+                  <Row>
+                    <Col xs="6" sm="4">
+                      <a href="#" id="LikeButton">
+                        <Button block size="sm" outline color="primary" onClick={() => this.onSendLikeBtnClick(this.props.user_id, companion.id)}>
+                          <img src={require('../../../../Resources/Like.png')} height="100" width="100" alt={'Like!'} />
+                        </Button>{' '}
+                      </a>
+                      <UncontrolledTooltip target="LikeButton">좋아요!</UncontrolledTooltip>
+                    </Col>
+                    <Col xs="6" sm="4">
+                      <a href="#" id="MessageButton">
+                        <Button block size="sm" outline color="primary" onClick={() => this.onSendMessageBtnClick()}>
+                          <img src={require('../../../../Resources/message.png')} height="100" width="100" alt={'Send Message!'} />
+                        </Button>{' '}
+                      </a>
+                      <UncontrolledTooltip target="MessageButton">메세지 보내기</UncontrolledTooltip>
+                    </Col>
+                    <Col sm="4">
+                      <a href="#" id="ProposalButton">
+                        <Button block size="sm" outline color="primary" onClick={() => this.onSendProposalBtnClick(this.props.user_id, companion.id)}>
+                          <img src={require('../../../../Resources/heartbone.png')} height="100" width="100" alt={'Proposal!'} />
+                        </Button>
+                        <MessageApp
+                          messageAppOpen={this.state.messageAppActivated}
+                          messageSenderId={this.props.user_id}
+                          messageReceiverName={companion.name}
+                          messageReceiverId={companion.id}
+                          messageToggle={() => this.onSendMessageBtnClick()}
+                          messageSend={this.props.post_message}
+                        />
+                      </a>
+                      <UncontrolledTooltip target="ProposalButton">결혼해요❤️</UncontrolledTooltip>
+                    </Col>
+                  </Row>
+                </div>
+              </Card>
+            </Col>
+          </Jumbotron>
+        </div>
+      )
+    }
+    else {
+      return (
+        <Jumbotron className="container">
+          멍멍!
+        </Jumbotron>
+      )
+    }
+  }
 }
