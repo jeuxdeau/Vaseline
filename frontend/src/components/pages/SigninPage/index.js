a3ecdb3d443823d5a58c63e593c6ac3bb789cc00
jeuxdeau <keyxiz@gmail.com>
2018-06-19 09:32:25 +0900

    complete signin with css

--- frontend/src/components/pages/SigninPage/index.js
+++ frontend/src/components/pages/SigninPage/index.js
@@ -29,52 +29,22 @@ export default class SigninPage extends Component {
 	render() {
 		const errors = this.props.errors || {}
 		return (
-			<div> // 큰 div
 			<div class="login-page">
 			  <div class="form">
 			    <form class="login-form" onSubmit={this.onSubmit}>
-			      <input type="text" name="username" placeholder="username" onChange={this.handleInputChange}/>
-			      <input type="password" name="password" placeholder="password" onChange={this.handleInputChange}/>
-			      <button type="submit">login</button>
-			      <p class="message">Not registered? <a href="/signup">Create an account</a></p>
+				{
+					errors.non_field_errors?
+ 					<Alert color="warning">
+ 						{errors.non_field_errors}
+ 					</Alert>: ""
+ 				}
+				<TextInput name="username" placeholder="id" error={errors.username} onChange={this.handleInputChange} />
+				<TextInput name="password" placeholder="password" error={errors.password} type="password" onChange={this.handleInputChange} />
+				<button type="submit">login</button>
+			    <p class="message">Not registered? <a href="/signup">Create an account</a></p>
 			    </form>
 			  </div>
 			</div>
-
-			<Jumbotron className="container">
-				<Form class="form" onSubmit={this.onSubmit}>
-					<h1>VASELINE</h1>
-					{
-						errors.non_field_errors?
-							<Alert color="danger">
-								{errors.non_field_errors}
-							</Alert>: ""
-					}
-					<InputGroup>
-						<InputGroupAddon addonType="prepend">
-	  					<Button color="danger">아이디♥</Button>
-						</InputGroupAddon>
-						<Input name="username" label="Username" error={errors.username} onChange={this.handleInputChange} />
-					</InputGroup>
-
-					<InputGroup>
-						<InputGroupAddon addonType="prepend">
-						<Button color="danger">비밀번호</Button>
-						</InputGroupAddon>
-						<Input name="password" label="Password" error={errors.password} type="password"
-								onChange={this.handleInputChange} />
-					</InputGroup>
-
-					<Button type="submit" color="secondary" size="lg">
-						Log in
-					</Button>
-					&nbsp;
-					<Button color="info" size="lg" tag={Link} to='/signup'>
-						Sign Up
-					</Button>
-				</Form>
-			</Jumbotron>
-			</div> // 큰 div
 		)
 	}
 }
